a5d63a9bd5d9291adaf2690bc6c3997cb8735714
michelguilhen <guilhenmichel@gmail.com>
2023-10-04 11:08:12 -0600

    initial

---
+++ src/BoilerPlateWithRepos.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace BoilerPlateWithRepos.Api.Endpoints
+{
+    public static class GamesEndpoints
+    {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("api/games", () =>
+            {
+                return new string[] { "Elden Ring", "StarCraft" };
+            })
+            .WithName("Get Games")
+            .WithOpenApi();
+        }
+    }
+}
